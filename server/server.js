b0bb63046024aeae5921b8b41a00aea2dc1723be
Alastair Ng <yusenng88@gmail.com>
2023-08-26 15:37:49 +0800

    chore: implemented middleware to log req statement

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
+require('dotenv').config()//attach those environment for us in the object
+
 const express = require('express')
 
 //express app 
 const app = express() 
 
+//Middleware, we need the next to go on to the next middleware
+// this middleware help us log all the request that are coming.
+app.use((req, res, next) =>{
+    console.log(req.path, req.method)
+    next()
+})
+
+//req = respond to request of the object, res = send out response to then browser
+app.get('/', (req,res) =>{
+    res.json({msg: 'Welcome to the app'}) //send a json string for us
+})
+
 //listen for requests
-app.listen(4000,() => {
-    console.log('listening on port 4000!!!!')
+//use process.env to access the file from the env file(hidden file)
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
 
 })
 
