5da32fbd74f57af8d421902b3b537a67804f4b07
Alastair Ng <yusenng88@gmail.com>
2023-08-26 13:48:04 +0800

    first commit

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -19,7 +19,7 @@ import Logo from '~/svg/Logo.svg';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
+// to customize the default configuration. what the heck
 
 export default function HomePage() {
   return (
